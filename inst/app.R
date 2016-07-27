31ece6eb5a8c801012608040a066d3332dd14da0
Roger Day <notsofeo@gmail.com>
2016-07-27 12:35:39 -0400

    shhorter names for the parasite example. We can't have I_cide as a node and use LoopAnalyst.

--- inst/app.R
+++ inst/app.R
@@ -13,12 +13,13 @@ modelStringList = c(
   'a -( a     a )-> b     b )-> p1     b )-> p2      p1 )-( p2 #Two predators, positive feedback',
   'x1 )-> x2  x2 )-( x3 x3 ->x1 x3 -( x3 # Levins 1974 fig3A ',
   'Qout-(Qout    Pressure-( Pressure    Depth-> Pressure  Pressure ->Qout  Qout-(Depth  ### Denver Dash bathtub',
-  'Plant1 -( Plant1    Plant1 )-> Hvore1    Hvore1 )-> Pred
-   Plant2 -( Plant2    Plant2 )-> Hvore2    Hvore2 )-> Pred
-   Hvore1 )-> Parasite
-     ### Levins 1974 Fig 5 '
-### I_cide -( Parasite   I_cide -( Hvore1   I_cide -( Pred   I_cide -( I_cide
-###  But I_cide as a node doesnt work.
+  'Pt1 -( Pt1    Pt1 )-> Hv1    Hv1 )-> Pred
+   Pt2 -( Pt2    Pt2 )-> Hv2    Hv2 )-> Pred
+   Hv1 )-> Para
+     # Levins 1974 Parasite Fig 5 '
+    ####I -( Para   I -( Hv1   I -( Pred   I -( I
+    ### I_cide -( Parasite   I_cide -( Hvore1   I_cide -( Pred   I_cide -( I_cide
+    ###  But I_cide as a node doesnt work.
 )
 
 
