2bef904478b1ec97a2c25ced06467c7017d055f0
Roger Day <notsofeo@gmail.com>
2016-07-29 10:49:28 -0400

    For deploying our app to the web, at https://trials.shinyapps.io/LoopModelExplorer

---
+++ inst/LoopModelExplorer/deploy.R
@@ -0,0 +1,28 @@
+.deploy = function(project="LevinsLoops",
+                   app="LoopModelExplorer", reInstall=TRUE){
+  ## TODO: first check that the html files are created committed and pushed.
+  if(reInstall)
+    .installFromGithub(project)
+  apps = app
+  for (app in apps) {
+    if(substr(app, 1, 5) == "inst/")
+      warning(".deploy: do not include 'inst' in app name.")
+    cat("wd is " %&% getwd() %&% "\n")
+    cat("wd changing to " %&% "inst/" %&% app %&% "\n")
+    setwd("inst/" %&% app)
+    tryCatch({
+      require("shinyapps")
+      deployApp()
+    },
+    finally={
+      cat("shinyapps::showLogs(appDir = 'inst/" %&% app %&% "')\n")
+      setwd("../..")}
+    )
+  }
+}
+
+.runDeployed = function(app="LoopModelExplorer/"){
+  system("open https://trials.shinyapps.io/" %&% app)
+  cat("shinyapps::showLogs(appDir = 'inst/" %&% app %&% "')\n")
+}
+
