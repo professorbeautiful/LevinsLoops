3813be6adf6302c303ca91d4d73caa6789e58d03
Roger Day <notsofeo@gmail.com>
2016-08-10 13:38:00 -0400

    stringToCM now works using "o"s too. E.g. o-> instead of )->.  But the arg use_o=T is not yet used elsewhere.

--- R/stringToCM.R
+++ R/stringToCM.R
@@ -1,42 +1,56 @@
-#'stringToCM
+#' stringToCM
 #'
-#'Conversion from a loop string to a community matrix.
+#' Conversion from a loop string to a community matrix.
 #'
 #' @param linkstring String of links separated by spaces and/or newlines. Each link connects two nodenames with "->" ")->" etc.
 #' @return An NxN community matrix, where N=# distinct nodes.
 #' @examples{
 #' stringToCM(
-#'    "a )-> b
-#'     b )-> c
-#'        a -( a")
+#'    "a )-> b     b )-> c
+#'        a -( a" )
 #'  }
 
-stringToCM = function(linkstring="a->b a-(a b-(a") {
-  theComment = gsub('.*#', "", linkstring)
+stringToCM = function(linkstring="a->b a-(a b-(a", use_o = FALSE,
+                      o_rh = "\\(", o_lh = "\\)",
+                      sep=" ") {
+  # We need to deal with ambiguity of   xyo->z.
+  # Is "o" part of the name, or the connector?
+  # For now, detect and throw error.
+  if(use_o)
+    o_rh = o_lh = "o"
+  ambiguity = (o_lh=="o" & length(grep("[^ ]o-", linkstring))>0 ) |
+    (o_rh=="o" & length(grep("-o[^ ]o", linkstring))>0 )
+  if(ambiguity)
+    stop("Please resolve ambiguity: o in name or in connector?")
+  theComment = gsub('.*#*', "", linkstring)
   linkstring = gsub('#.*', "", linkstring)   # remove comments
-  linkstring = gsub("[ \n]+", " ", linkstring) # Remove newlines
-  # the next 2 lines remove  spaces,
-  #  allow user to put in spaces for readability.
-  linkstring = gsub(" *([\\)<]*-[\\(>]*) *", "\\1", linkstring)
+  linkstring = gsub("[ \n]+", sep, linkstring)
+  # Replace newlines by word separator.
+  # the next 2 lines remove spaces,
+  #  Remove spaces inside each word.
+  linkstring = gsub(
+    paste0(" *([",o_lh, "<]*-[", o_rh, ">]*) *"),
+    "\\1", linkstring)
   #Now, handle the ")->" and "<-(" cases:
   links  = strsplit(x = linkstring, split = "[ \n]")[[1]]
-  is_two_way = grep(pattern = "[\\)<]-[\\(>]", links)
+  two_way_pattern = paste0("[", o_lh, "<]-[", o_rh, ">]")
+  is_two_way = grep(pattern = two_way_pattern, links)
   if(length(is_two_way) > 0) {
     # replace each two-way link element by the two links it represents.
-    links = c(links, sub("[\\)<]-", "-", links[is_two_way]))
-    links = c(links, sub("-[\\(>]", "-", links[is_two_way]))
+    links = c(links, sub(paste0("[", o_lh, "<]-"), "-", links[is_two_way]))
+    links = c(links, sub(paste0("-[", o_rh, ">]"), "-", links[is_two_way]))
     # Remove these link elements
     links = links[-is_two_way]
   }
-  linknodes <- strsplit(links, "[<\\)]*-[>\\(]*")
-  names = unique(unlist(strsplit(links, "->|-\\(|<-|\\)-")))
+  linknodes <- strsplit(links, paste0("[<", o_lh, "]*-[>", o_rh, "]*") )
+  names = unique(unlist(strsplit(links, paste0("->|-", o_rh, "|<-|", o_lh, "-"))))
   n = length(names)
   cm = matrix(rep(0,n^2), nrow=n, dimnames=list(names,names))
   signs = rep(-1, length(links))
   signs[grep("->|<-", links)] = 1
   from_to = as.matrix(as.data.frame(linknodes, stringsAsFactors=FALSE))
   ## Reverse any pointing left instead of right.
-  backwards = (regexpr("[\\)<]-", links) > -1)
+  backwards = (regexpr(paste0("[", o_lh, "<]-"), links) > -1)
   if(any(backwards))
     from_to[ , backwards] = apply(from_to[ , backwards, drop=F], 2, rev)
   from = from_to[1, ]
@@ -47,38 +61,6 @@ stringToCM = function(linkstring="a->b a-(a b-(a") {
   return (cm)
 }
 
-
-stringToCM(
-"a->b
-a-(a
-b-(a")
-stringToCM(
-  "a)->b
-b )-> c
-  a-(a")
-
-RHxy =
-"R-(R
-R->H
-H-(R
-H->x
-x-(H
-H->y
-y-(H
-y-(y"
-
-stringToCM(RHxy)
-
-ABCDE=
-"A-(A
-A->B
-B-(A
-B->C
-C-(B
-C->D
-D-(C
-D->E
-E->D"
 
 #' CMtoIPMnet
 #'
@@ -113,7 +95,3 @@ CMtoIPMnet = function(cm) {
   )
 }
 
-CMtoIPMnet(stringToCM(ABCDE))
-
-library("LoopAnalyst")
-LoopAnalyst::make.cem(stringToCM(ABCDE))
