3f7c55bef81feb32598ea1f3e461cf0958ff8318
Roger Day <notsofeo@gmail.com>
2016-08-17 16:34:26 -0400

    Working towards a useful feedback_K function. Might be done. Needs testing. Not efficient of course.

--- R/feedback_K.R
+++ R/feedback_K.R
@@ -1,26 +1,40 @@
+getLoopValue = function(loop, M) {
+  print(loop)
+  prod(
+    sapply(1:(length(loop)-1), function(ind) M[loop[ind], loop[ind+1]])
+  )
+}
+
 feedback_K = function(M) {
   loops = LoopAnalyst::enumerate.loops(M)
   nLoops = length(loops)
-  loopLengths = sapply(loops, length)
-  disjointSets = list(loops)  ### start with M=1
-  disjointNext = lapply(disjointSets,
-                        function(set){
-
-
-  })
-
-  findDisjoint = function(rootSet, maxIndex) {
-    repeat {
-    }
-  }
-  possible = loops[[1]]
-  for(L1 in 1:(nLoops-1))
-    for(L2 in (L1+1):nLoops)
-      if(
-        length(intersect(loops[[L1]],loops[[L2]]))==0)
-        disjointSets[[length(disjointSets)+1]] =
-                         list(L1=loops[[L1]], L2=loops[[L2]],
-                              loopLengths[L1], loopLengths[L2])
-    disjointSets
+  choppedLoops = lapply(loops, '[', -1)
+  loopLengths = sapply(loops, length) - 1
+  powerSet = expand.grid(rep(list(c(FALSE,TRUE)), length(loops))) [ -1, ]
+  t.powerSet = as.data.frame(t(powerSet))  # each column represents a loop set.
+  loopSetIsDisjoint = sapply(t.powerSet, function(set)
+    sum(duplicated(unlist(choppedLoops[which(set)]))) == 0 )
+  loopSetStrings = sapply(t.powerSet, function(set)
+    paste(choppedLoops[which(set)], collapse=",") )
+  nLoopsPerSet = colSums(t.powerSet)  ### this is "m".
+  nVarsPerSet = sapply(t.powerSet, function(set) # this is "k".
+    length(unique(Reduce(union, choppedLoops[which(set)]))) )
+  completeByLoopSet =  (nVarsPerSet == nrow(M))
+  lengthsByLoopSet = unlist(apply(powerSet, 1, function(row) {
+    unique(
+      Reduce(sum, loopLengths[which(row)]))
+  }))
+  loopValues = sapply(loops, getLoopValue, M=M)
+  setValues = sapply(t.powerSet, function(set) prod(loopValues[which(set)]))  ### Lmk.
+  print(allData <- data.frame(loopProdStrings, nL=nLoopsPerSet, nV=nVarsPerSet, Lmk=setValues,
+                   complete=completeByLoopSet, disj=loopSetIsDisjoint
+             ))
+  ## Lmk = L(m, k) is a product of m disjunct loops totaling k elements.
+  Fk = sapply(1:nrow(M), function(k)
+    with(allData[allData$nV==k & allData$disj, ],
+         sum((-1)^(nL+1) * setValues)
+    )
+  )
+  return(Fk)
 }
 if(interactive()) feedback_K(cm.levins)
