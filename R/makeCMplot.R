35260ca480e72a8742e5e0631ed81584e51242f0
Roger Day <notsofeo@gmail.com>
2016-08-17 13:54:26 -0400

    "size" arg added to makeCMplot, for resizing figure in Rmarkdown.

--- R/makeCMplot.R
+++ R/makeCMplot.R
@@ -1,15 +1,19 @@
-makeCMplot = function(CM) {
+makeCMplot = function(CM, return='auto',   changeArrows = F, size) {
   dotFileCM = 'M.graphcm.dot'
   graph.cm(CM, file=dotFileCM)
   ### Replace the "odot" circle for negative link by "tee" or "odiamond" or "invempty"
-  changeArrows = F
   if (changeArrows) {
     system("sed s/odot/invempty/ > M.graphcm.fixed.dot < M.graphcm.dot")
-    dotFileCM = 'M.graphcm.fixed.dot'
+    system("cp M.graphcm.fixed.dot M.graphcm.dot")
   }
+  if(!missing(size)) {
+    system(paste0("sed s/18!/", size, "/g > M.graphcm.fixed.dot < M.graphcm.dot"))
+    system("cp M.graphcm.fixed.dot M.graphcm.dot")
+  }
+  dotFileCM = 'M.graphcm.dot'
   outfile = paste0(dotFileCM, ".svg")
-  dot(DOT = paste(collapse=" ",
+  DOT::dot(DOT = paste(collapse=" ",
                   readLines(dotFileCM)),
-      file=outfile)
+      file=outfile, return=return)
   return(outfile)
 }
