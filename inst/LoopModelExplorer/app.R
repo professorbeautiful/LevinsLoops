8fc5762a9734c1e13c271db7a19d7fe3edc22a9e
Roger Day <notsofeo@gmail.com>
2017-04-12 19:41:47 -0400

    Brought in diffEq package. Sourcing modelStringList.R; reduces need to rebuild package.

--- inst/LoopModelExplorer/app.R
+++ inst/LoopModelExplorer/app.R
@@ -2,8 +2,11 @@ require(LevinsLoops)
 require("LoopAnalyst")
 require(shinyDebuggingPanel)
 require(DOT)
+require(diffEq)  ## rmutil deSolve pracma seem
+
 ### this is a comment
 data("cm.levins", package="LoopAnalyst")
+source("../../R/modelStringList.R")
 rValues = reactiveValues(CM=cm.levins, CM_qual = cm.levins,
                          modelStringModified = FALSE,
                          constantsDefault=c(1000, rep( -200, 4)),
